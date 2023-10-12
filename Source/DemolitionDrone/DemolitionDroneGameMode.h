7e85f52857cfcbdfb6d4880c7a7174873f81e3e4
Daniel_Zinayev <danilzinayev@gmail.com>
2023-10-12 20:11:42 +0300

    Unreal Engine 5.2.1 Third Person C++ Project

---
+++ Source/DemolitionDrone/DemolitionDroneGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DemolitionDroneGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADemolitionDroneGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADemolitionDroneGameMode();
+};
+
+
+
